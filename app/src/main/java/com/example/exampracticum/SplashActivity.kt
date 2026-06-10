0c72f2e3dba1453706d9058c8a837d3d7550e3b8
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-10 09:19:28 +0200

    Create SplashActivity
    Declarations

---
+++ app/src/main/java/com/example/exampracticum/SplashActivity.kt
@@ -0,0 +1,9 @@
+package com.example.exampracticum
+
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import android.widget.TextView
