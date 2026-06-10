58a4740bb0e921b13fcebe167d72617617c16c71
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-10 09:21:57 +0200

    Update SplashActivity
    Declarations

--- app/src/main/java/com/example/exampracticum/SplashActivity.kt
+++ app/src/main/java/com/example/exampracticum/SplashActivity.kt
@@ -7,3 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import android.widget.TextView
+//Declarations
+    private lateinit var tvTitle: TextView
+    private lateinit var SubTitle: TextView
+    private lateinit var btnStart: Button
+
+
+
+}}
