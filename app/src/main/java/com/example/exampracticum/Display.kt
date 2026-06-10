adb0f572e2b7d2f2b89f682b0c9ef10d975973d0
Lwandise248 <ST10487940@myemeris.edu.za>
2026-06-10 11:15:35 +0200

    Create Display.kt
    Declarations

---
+++ app/src/main/java/com/example/exampracticum/Display.kt
@@ -0,0 +1,7 @@
+class Display : AppCompatActivity {
+//Declarations
+    private lateinit var btnDisplay: Button
+    private lateinit var btnBack : Button
+    private lateinit var tvTitle : TextView
+
+
